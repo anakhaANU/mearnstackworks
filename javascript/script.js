57f71b40a0a40b5dfe3692928103a2936e68dab5
anakha <anakharajendran2000@gmail.com>
2021-09-09 01:59:55 -0400

    msg

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+// javascript programming
+//print(python)
+//printf(c)
+//system.out.println(java)
+//cout<<(c++)
+//console.log()=>javascript(for print message)
+//..........identifiers............
+//name are called identifiers(for identification purpose)
+//int age=27; age=>variable name(lower case in case of js noun)
+// use emp_name in case of js(2 words separate use_)
+//fntn display() displayValues=>fntn name(2 words are there so 2nd word first latter should be capital)
+//print_person() snake notation in python
+//printPerson() camel case in javascript
+//class person{}=>
+
+
+
+
+
+
+//variables
+//1.for storing
+//2.to represent a memory location
+//eg: int num=2;(here num is the variable and the value 2 is store as binary)
+//var,let,const(variable declaration use cheyyunna variables)
+//var=>value can be changed
+//const=>can't change value
+
+// console.log("hello world");
+// console.log("hello world");
+var company="luminar technolab";
+var loc_name="kakkanad";
+console .log(loc_name);
+//age,name
+//hai i am name age years old
